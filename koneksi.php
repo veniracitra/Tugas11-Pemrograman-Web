931617eae46369bf5c3805dd1e2f505766e309e8
veniracitra <100218385+veniracitra@users.noreply.github.com>
2022-06-15 18:47:16 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "db_penjualan";
+$koneksi    = mysqli_connect($host, $user, $password, $database) or die("Koneksi gagal terhubung".$database.mysqli_connect_error()) ;
+
+?>
